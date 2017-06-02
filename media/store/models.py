afafc35f1d5ed61bee7f011656a56a99aad25247
Amanda Cavallaro <amandacav@gmail.com>
2017-06-02 10:14:50 -0400

    new model and urls

---
+++ media/store/models.py
@@ -0,0 +1,15 @@
+"""
+All data models for Media Store
+"""
+
+from django.db import models
+
+# Create your models here.
+
+#class Service(models.Model):
+
+
+#class ServiceSubType(models.Model):
+
+# do we just copy this from Resource Matrix since we are trying to stay similar?
+
