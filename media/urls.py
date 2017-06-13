0f7ad336128644a13e87893225133b4019f139ad
Amanda Cavallaro <amandacav@gmail.com>
2017-06-13 13:49:27 -0400

    added dept, inventory, vendor, orders; began urls but have them commented out

--- media/urls.py
+++ media/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.conf import settings
 #from django.views.generic import TemplateViews
 
-from store import views #can add more here with , 
+from store import views #as store_views  #can add more here with , 
 
 #urlpatterns = [
 #	url(r'^store/', include('store.urls')),
@@ -27,16 +27,36 @@ from store import views #can add more here with ,
 #]
 app_name = 'store'
 urlpatterns = [
+
+#home page and login
+#	url(r'^$', store_views.home, name = 'home'),
+
 	url(r'^admin/', include(admin.site.urls)),
 #	url(r'^store/', view.index, name='home'),
-#	url(r'^login/', views.login, name='login'),
+#	url(r'^login', 'store_views.login', name='login'),
+#	url(r'^logout', 'store_views.logout'),
+#	url(r'^invalid', 'store_views.invalid_login'),
+#	url(r'^hijack/', include('hijack.urls')), #to hijack other users
+
 	url(r'^$', views.index, name='store'),
 
 
 
 
 
-#services
-#	url(r'^service/$', 'media')	
+#Inventory
+#	url(r'^inventory/$', 'store_views.inventory', name = 'Inventory'),
+#	url(r'^inventory/new', 'store_views.create_inventory', name = 'create_inventory'),
+#	url(r'^inventory/(?P<id>[0-9]*)$', 'store_views.single_item', name = 'single_item'),
+#	url(r'^inventory/(?P<id>[0-9]*)$', 'store_views.edit_single_item', name = 'edit_single_item'),
+
+#add link to dump to Resource Matrix here
+
+
+#Admin Interface
+#	url(r'^admin_dashboard/$*', 'store_views.admin_dashboard', name = 'admin_dashboard'),
+#	url(r'^order_delete', 'store_views.delete_order', name = 'delete_order')"""
+
+
 ]
 
--- store/models.py
+++ store/models.py
@@ -61,198 +61,101 @@ class Job(models.Model):
 	date_modified = models.DateTimeField(auto_now=True)
 #	job_class = models.CharField(max_length=30,blank=False)
 
+#WTF----------------------------------------------------------------------------------------
+class Department(models.Model):
+	name = models.CharField(max_length=100)
+	number = models.CharField(unique=True, max_length=6)
+	account_code = models.IntegerField(null=True, blank=True)
+	is_shared_resource = models.BooleanField(default=False)
+	active = models.BooleanField(default=True)
+
+#	objects = ActiveDepartmentManager()
+#	all_objects = models.Manager()
+
+	def __unicode__(self):
+		return self.number + " " + self.name
+
+	class Meta:
+		ordering = ('number',)
+
+
+"""class UserProfile(models.Model):
+#	user = models.ForeignKey(User, related_name='user_profile')
+#	department = models.ForeignKey(Department, blank=True, null=True)
+#	alt_departments = models.ManyToManyField(Department, related_name='alt_departments', blank=True, null=True)
+	hhmi_project_id = models.CharField(max_length=30, blank=True, null=True)
+	employee_id = models.CharField(max_length=20, blank=True, null=True)
+	email_address = models.CharField(max_length=255, blank=True, null=True)
+	first_name = models.CharField(max_length=30, blank=True, null=True)
+	last_name = models.CharField(max_length=30, blank=True, null=True)
+#	manager = models.ForeignKey(User, related_name='user_manager', blank=True, null=True, on_delete=models.SET_NULL)
+	is_manager = models.BooleanField(default=False)
+	is_active = models.BooleanField(default=False)
+	is_janelia = models.BooleanField(default=False)
+	is_visitor = models.BooleanField(default=False)
+
+	skip_updates = models.BooleanField(default=False)
+
+
+	is_privileged = models.BooleanField(default=False)
+
+
+	offboard_date = models.DateField(blank=True, null=True)
+
+	def __unicode__(self):
+		return str(self.user)
+
+	def department_id_list(self):
+		deparment_ids = [dept.id for dept in self.alt_departments.all()]
+		deparment_ids.append(self.department_id)
+		return deparment_ids
+
+	def name(self):
+		return self.last_name + ", " + self.first_name
+
+	def manager_name(self):
+		if self.manager:
+			return ' '.join([self.manager.first_name, self.manager.last_name])
+		return ''
+
+	def has_job_privileges(self):
+		return (self.user.is_superuser or
+			(self.is_manager and self.department.number == '093098'))"""
+
+
+
+class Vendor(models.Model):
+	email_address = models.CharField(max_length=225)
+
+
+class Inventory(models.Model):
+	class Meta:
+		verbose_name_plural = 'Inventory'
+
+	inventory_text = models.CharField(max_length=75)
+	cost = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+	date_created = models.DateField(auto_now_add=True)
+#NEW	date_modified = models.DateTimeField(auto_now=True)
+	notes = models.CharField(max_length=500, blank=True)
+	vendor = models.ForeignKey(Vendor, blank=False, null=False)
+#	deposit = models.
+
+
+class Order(models.Model):
+	inventory = models.ForeignKey(Inventory, blank=False, null=False)
+#	submitter = models.ForeignKey(User, related_name='submitter')
+#	department = models.ForeignKey(Department, blank=False, null=False)
+	special_instructions = models.TextField(blank=True)
+	date_created = models.DateField(auto_now_add=True)
+	date_modified = models.DateField(blank=True, null=True)
+	date_submitted = models.DateField(blank=True, null=True)
+	date_complete = models.DateField(blank=True, null=True)
+
+
+		
 
 
 
 
+			
 
-"""
-class WorkOrder(models.Model):
-    bill_to = models.ManyToManyField('Department',through='WorkorderDepartmentRelationship', related_name='workorders', blank=False, null=True)
-    job = models.ForeignKey(Job,blank=True,null=True)
-    phase = models.ForeignKey(Phase,blank=True,null=True)
-    submitter = models.ForeignKey(User,related_name='submitter')
-    main_requestor = models.ForeignKey(User,related_name='main_requestor')
-    department = models.ForeignKey(Department,blank=False,null=True)
-    note = models.TextField(blank=True)
-    status = models.CharField(max_length=30,blank=False,null=False, default='Complete',
-       choices=(
-        ('Submitted','Submitted'),
-        ('Assigned','Assigned'),
-        ('In_Progress','In Progress'),
-        ('Complete','Complete'),
-        ('Canceled','Canceled'),
-        ('Inquiry','Inquiry'),
-       )
-    )
-    date_complete = models.DateField(blank=False)
-    test_field = models.CharField(max_length=30,blank=True,null=True)
-    job_manager_id = models.CharField(max_length=30,blank=True,null=True)
-    date_promised = models.DateField(blank=True,null=True)
-    date_revised = models.DateField(blank=True,null=True)
-    date_submitted = models.DateField(blank=True,null=True)
-    admin_code = models.CharField(max_length=30,blank=True)
-    date_billed = models.DateField(blank=True,null=True)
-    objects = WorkOrderManager()
-    date_modified = models.DateTimeField(auto_now=True)
-    # stores the id related to the reservation in resource scheduler.
-    # this helps prevent the duplication of tickets and allows us to
-    # easily link back to the other site.
-    resource_scheduler_id = models.CharField(max_length=255, blank=True)
-
-    def already_billed(self):
-        if self.date_billed:
-            return True
-        return False
-
-    def total(self):
-        total = 0
-        list = self.workorderline_set.all()
-        for line in list:
-            total += line.total()
-        return total
-
-    def performed_by(self):
-        user = ''
-        first_line = self.workorderline_set.first().performed_by
-
-        if first_line:
-            user = first_line.username
-
-        return user
-
-    def is_self_billing(self):
-        if self.department in self.bill_to.all():
-            return True
-        return False
-
-    def bill_to_dept_count(self):
-        return self.workorderdepartmentrelationship_set.count()
-
-    def reclass(self, new_data):
-        original_id = self.id
-        original_lines = self.workorderline_set.all()
-        original_depts = self.workorderdepartmentrelationship_set.all()
-        original_note = self.note
-
-        # create a workorder to credit the original workorder departments.
-        credit_wo = self
-        credit_wo.pk = None
-        credit_wo.date_complete = datetime.date.today()
-        credit_wo.date_billed = None
-        credit_wo.note = original_note + "\nReclass credit from workorder {0}".format(original_id)
-        credit_wo.save()
-        credit_id = credit_wo.id
-
-        # restore the department relationships.
-        for rel in original_depts:
-            fields = WorkorderDepartmentRelationship._meta.get_all_field_names()
-            new_rel = WorkorderDepartmentRelationship()
-            # must ignore id or the original relationship will get overwritten
-            ignored = ('workorder','id')
-            for field in fields:
-                if field in ignored:
-                    continue
-                setattr(new_rel, field, getattr(rel, field))
-
-            new_rel.workorder_id = credit_wo.id
-            new_rel.save()
-
-        # create negative lines for each original line
-        for line in original_lines:
-            fields = WorkOrderLine._meta.get_all_field_names()
-            new_line = WorkOrderLine()
-            ignored = ('wo_id','id')
-            for field in fields:
-                if field in ignored:
-                    continue
-                setattr(new_line, field, getattr(line, field))
-            new_line.wo_id = credit_wo
-            new_line.cost = -new_line.cost
-            new_line.save()
-
-        # create a workorder to debit the new departments.
-        debit_wo = self
-        debit_wo.pk = None
-        debit_wo.date_complete = datetime.date.today()
-        debit_wo.date_billed = None
-        debit_wo.note = original_note + "\nReclass debit from workorder {0}".format(original_id)
-        debit_wo.save()
-        debit_id = debit_wo.id
-
-        # restore the lines for each original line
-        for line in original_lines:
-            new_line = line
-            new_line.pk = None
-            new_line.wo_id = debit_wo
-            new_line.save()
-
-        # create the new department relationships.
-        for entry in new_data:
-            if not entry['DELETE']:
-                billing = WorkorderDepartmentRelationship(
-                    workorder = debit_wo,
-                    department_id = entry['new_dept'],
-                    hhmi_project_id = entry['new_hhmi_project_id'],
-                    percentage = entry['new_percentage']
-                )
-                billing.save()
-
-        return(credit_id, debit_id)
-
-    def __unicode__(self):
-        return str(self.pk)
-
-class WorkorderDepartmentRelationship(models.Model):
-    workorder  = models.ForeignKey(WorkOrder, blank=False)
-    department = models.ForeignKey(Department,blank=False)
-    percentage = models.PositiveSmallIntegerField(default=100,blank=False)
-    hhmi_project_id = models.CharField(max_length=255,blank=True,null=True)
-
-    # some times we store the name of the person associated with the project id
-    # this is no good for finace, so this function will strip it off it is present
-    # and only return the 9 digit code.
-    def short_hhmi_project_id(self):
-        if self.hhmi_project_id:
-            return self.hhmi_project_id.split(' ', 1)[0]
-        else:
-            return None
-
-    def __unicode__(self):
-        return str(self.pk)
-
-
-class WorkOrderLine(models.Model):
-    wo_id = models.ForeignKey(WorkOrder)
-    description = models.TextField(blank=True)
-    line_type = models.CharField(max_length=255)
-    date_performed = models.DateField(blank=True, null=True)
-    performed_by = models.ForeignKey(User,related_name='performed_by',blank=True,null=True)
-    qty = models.DecimalField(max_digits=10,decimal_places=2, default=0)
-    unit = models.CharField(max_length=30,blank=True,null=True)
-    cost = models.DecimalField(max_digits=10,decimal_places=2, default=0)
-    service = models.ForeignKey(Service,blank=True, null=True, on_delete=models.SET_NULL)
-    category = models.CharField(max_length=30, null=True, choices = CATEGORY_CHOICES)
-    def total(self):
-        total = 0.00
-        if self.cost and self.qty:
-            total = round(decimal.Decimal(str(self.qty))*decimal.Decimal(str(self.cost)),2)
-        return decimal.Decimal(total)
-
-    def category_choices(self):
-        return CATEGORY_CHOICES
-
-    class Meta:
-        verbose_name_plural = "work order lines"
-
-    def __unicode__(self):
-        return u'%s' % self.pk
-        """
-
-
-
-
-#	category = models.CharField(max_length=30,blank=True,null=True,choices = CATEGORY_CHOICES)
-#note sure category choices should be the same but not sure if RM requires it to be the same?
-
-# do we just copy this from Resource Matrix since we are trying to stay similar?
