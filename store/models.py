dec6a2d9c1f1386d1548746794a08bd680f311e9
Amanda Cavallaro <amandacav@gmail.com>
2017-06-14 15:01:45 -0400

    update_users.py added and logo

--- store/models.py
+++ store/models.py
@@ -15,90 +15,100 @@ import datetime
 
 
 class Department(models.Model):
-	name = models.CharField(max_length=100)
-	number = models.CharField(unique=True, max_length=6)
-	account_code = models.IntegerField(null=True, blank=True)
-	is_shared_resource = models.BooleanField(default=False)
-	active = models.BooleanField(default=True)
+    name = models.CharField(max_length=100)
+    number = models.CharField(unique=True, max_length=6)
+    account_code = models.IntegerField(null=True, blank=True)
+    is_shared_resource = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
 
-#	objects = ActiveDepartmentManager()
-#	all_objects = models.Manager()
+#   objects = ActiveDepartmentManager()
+#   all_objects = models.Manager()
 
-	def __unicode__(self):
-		return self.number + " " + self.name
+    def __unicode__(self):
+        return self.number + " " + self.name
 
-	class Meta:
-		ordering = ('number',)
+    class Meta:
+        ordering = ('number',)
 
 
 """class UserProfile(models.Model):
-#	user = models.ForeignKey(User, related_name='user_profile')
-#	department = models.ForeignKey(Department, blank=True, null=True)
-#	alt_departments = models.ManyToManyField(Department, related_name='alt_departments', blank=True, null=True)
-	hhmi_project_id = models.CharField(max_length=30, blank=True, null=True)
-	employee_id = models.CharField(max_length=20, blank=True, null=True)
-	email_address = models.CharField(max_length=255, blank=True, null=True)
-	first_name = models.CharField(max_length=30, blank=True, null=True)
-	last_name = models.CharField(max_length=30, blank=True, null=True)
-#	manager = models.ForeignKey(User, related_name='user_manager', blank=True, null=True, on_delete=models.SET_NULL)
-	is_manager = models.BooleanField(default=False)
-	is_active = models.BooleanField(default=False)
-	is_janelia = models.BooleanField(default=False)
-	is_visitor = models.BooleanField(default=False)
+#   user = models.ForeignKey(User, related_name='user_profile')
+#   department = models.ForeignKey(Department, blank=True, null=True)
+#   alt_departments = models.ManyToManyField(Department, related_name='alt_departments', blank=True, null=True)
+    hhmi_project_id = models.CharField(max_length=30, blank=True, null=True)
+    employee_id = models.CharField(max_length=20, blank=True, null=True)
+    email_address = models.CharField(max_length=255, blank=True, null=True)
+    first_name = models.CharField(max_length=30, blank=True, null=True)
+    last_name = models.CharField(max_length=30, blank=True, null=True)
+#   manager = models.ForeignKey(User, related_name='user_manager', blank=True, null=True, on_delete=models.SET_NULL)
+    is_manager = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=False)
+    is_janelia = models.BooleanField(default=False)
+    is_visitor = models.BooleanField(default=False)
 
-	skip_updates = models.BooleanField(default=False)
-
-
-	is_privileged = models.BooleanField(default=False)
+    skip_updates = models.BooleanField(default=False)
 
 
-	offboard_date = models.DateField(blank=True, null=True)
+    is_privileged = models.BooleanField(default=False)
 
-	def __unicode__(self):
-		return str(self.user)
 
-	def department_id_list(self):
-		deparment_ids = [dept.id for dept in self.alt_departments.all()]
-		deparment_ids.append(self.department_id)
-		return deparment_ids
+    offboard_date = models.DateField(blank=True, null=True)
 
-	def name(self):
-		return self.last_name + ", " + self.first_name
+    def __unicode__(self):
+        return str(self.user)
 
-	def manager_name(self):
-		if self.manager:
-			return ' '.join([self.manager.first_name, self.manager.last_name])
-		return ''
+    def department_id_list(self):
+        deparment_ids = [dept.id for dept in self.alt_departments.all()]
+        deparment_ids.append(self.department_id)
+        return deparment_ids
 
-	def has_job_privileges(self):
-		return (self.user.is_superuser or
-			(self.is_manager and self.department.number == '093098'))"""
+    def name(self):
+        return self.last_name + ", " + self.first_name
+
+    def manager_name(self):
+        if self.manager:
+            return ' '.join([self.manager.first_name, self.manager.last_name])
+        return ''
+
+    def has_job_privileges(self):
+        return (self.user.is_superuser or
+            (self.is_manager and self.department.number == '093098'))"""
 
 
 
 class Vendor(models.Model):
-	email_address = models.CharField(max_length=225)
+    email_address = models.CharField(max_length=225, blank=True, null=True)
+    company = models.CharField(max_length=50)
+    contact = models.CharField(max_length=100, blank=True, null=True)
+    notes = models.CharField(max_length=500, blank=True, null=True)
+
+
 
 
 class Inventory(models.Model):
-	class Meta:
-		verbose_name_plural = 'Inventory'
+    class Meta:
+        verbose_name_plural = 'Inventory'
 
-	inventory_text = models.CharField(max_length=75)
-	cost = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-	date_created = models.DateField(auto_now_add=True)
-#NEW	date_modified = models.DateTimeField(auto_now=True)
-	notes = models.CharField(max_length=500, blank=True, null=True)
-	vendor = models.ForeignKey(Vendor, blank=True, null=True)
-#	deposit = models.
+    inventory_text = models.CharField(max_length=75)
+    cost = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    date_created = models.DateField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    notes = models.CharField(max_length=500, blank=True, null=True)
+    vendor = models.ForeignKey(Vendor, blank=True, null=True)
+    deposit = models.IntegerField(default=0, blank=True,null=True)
+    withdrawal = models.IntegerField(default=0, blank=True, null=True)
+    part_num = models.CharField(max_length=20, blank=True, null=True)
+    minimum_amt = models.IntegerField(max_length=1000, blank=True, null=True)
+    current_amt = deposit - withdrawal
+
 
 
 class Order(models.Model):
-	inventory = models.ForeignKey(Inventory, blank=False, null=False)
-#	submitter = models.ForeignKey(User, related_name='submitter')
-#	department = models.ForeignKey(Department, blank=False, null=False)
-	special_instructions = models.TextField(blank=True)
-	date_created = models.DateField(auto_now_add=True)
-	date_modified = models.DateField(blank=True, null=True)
-	date_submitted = models.DateField(blank=True, null=True)
-	date_complete = models.DateField(blank=True, null=True)
+    inventory = models.ForeignKey(Inventory, blank=False, null=False)
+#   submitter = models.ForeignKey(User, related_name='submitter')
+    department = models.ForeignKey(Department, blank=False, null=False)
+    special_instructions = models.TextField(blank=True)
+    date_created = models.DateField(auto_now_add=True)
+    date_modified = models.DateField(blank=True, null=True)
+    date_submitted = models.DateField(blank=True, null=True)
+    date_complete = models.DateField(blank=True, null=True)
