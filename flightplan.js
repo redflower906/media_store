8b0bd67a23110c39691fb4de9e209d5bc422e2aa
Harrison <harrisons1@hhmi.org>
2018-04-04 16:21:36 -0400

    flightplan

--- flightplan.js
+++ flightplan.js
@@ -20,7 +20,7 @@ var config = {
 
 plan.target('production', {
   host: '10.37.6.50', // vm650
-  username: os.userInfo(), //process.env.USER,
+  username: os.userInfo, //process.env.USER,
   agent: process.env.SSH_AUTH_SOCK,
 },
 {
