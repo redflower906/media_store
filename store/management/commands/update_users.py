f148ffe365da369f4960200f0fbab34bc143b24a
Harrison <harrisons1@hhmi.org>
2018-05-31 13:22:04 -0400

    trying to encode employees line 153

--- store/management/commands/update_users.py
+++ store/management/commands/update_users.py
@@ -150,8 +150,8 @@ def get_active_employees(emp_id=None):
         url += str(emp_id)
 
     res = requests.get(url)
-    res = res.encode('ascii')
     employees = json.loads(res.content)
+    employees = employees.encode('ascii')
     #filter out employees that have termination dates previous to 30 days ago
     def should_be_active(emp):
         if not emp['TERMINATIONDATE']:
