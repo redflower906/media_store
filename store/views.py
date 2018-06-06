d27526e12e70ea7d31a5392440512a9af73a529d
Harrison <harrisons1@hhmi.org>
2018-06-06 16:23:05 -0400

    editing create_order.html

--- store/views.py
+++ store/views.py
@@ -200,6 +200,7 @@ def create_order(request, copy_id=None):
 
     order = Order()
     user = request.user
+    username = user.userprofile.name
     print(request.user.userprofile.name)
     if request.method == "POST":
         
@@ -265,6 +266,7 @@ def create_order(request, copy_id=None):
         'inventory_lists': __build_inventory_groups(),
         'media_types': MEDIA_CHOICES,
         'user': user,
+        'username':username,
     })
 
 @login_required(login_url='login')
