f22f1173189c216a0d33fee7e4c6c219f3d9a5c0
Harrison <harrisons1@hhmi.org>
2018-06-06 07:18:27 -0400

    print all_employees

--- store/management/commands/update_users.py
+++ store/management/commands/update_users.py
@@ -155,7 +155,7 @@ def get_active_employees(emp_id=None):
     res = requests.get(url)
     #employees = json.loads(res.content)
     employees = res.json()
-    print (json.dumps(employees, indent=4)) #seems like all employees are found, not just visitors
+    # print (json.dumps(employees, indent=4)) #seems like all employees are found, not just visitors
     #filter out employees that have termination dates previous to 30 days ago
     def should_be_active(emp):
         if not emp['TERMINATIONDATE']:
@@ -557,7 +557,7 @@ class Command(BaseCommand):
         #'WORKERTYPE', 'LEGACYDEPTID', 'EMPLOYEEID', 'FIRSTNAME', 'LASTNAME', 'MGRLASTNAME',
         #'EMAILADDRESS', 'COSTCENTER', 'MGRFIRSTNAME', 'MGRID'}
         all_employees = get_active_employees(emp_id=emp_id)
-        print (emp_id)
+        print (all_employees)
         # build a lookup dict of the users in workday that should be active in ResourceMatrix
         in_workday = {}
         for emp in all_employees:
