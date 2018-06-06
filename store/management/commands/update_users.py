879bd0f5c0cdecdbc55c0aec6cc0fded6477ba31
Harrison <harrisons1@hhmi.org>
2018-06-06 07:49:16 -0400

    print deptname

--- store/management/commands/update_users.py
+++ store/management/commands/update_users.py
@@ -238,7 +238,7 @@ def get_department(deptid, deptname):
         dept.department_name = deptname
         dept.save()
         message("Created department with id {0}\n".format(deptid),'warning')
-
+    print (deptname)
     #make sure we are billing the correct department for Gerry
     if dept.number == 'CC51050':
         try:
