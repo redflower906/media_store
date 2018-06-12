a0a8811d83170e4918d8e80c00be51637dda2934
Harrison <harrisons1@hhmi.org>
2018-06-12 11:29:09 -0400

    trying to change requester dataset from username to userprofile.name

--- store/views.py
+++ store/views.py
@@ -206,7 +206,7 @@ def create_order(request, copy_id=None):
     
     if request.method == "POST":
         
-        order_form = OrderForm(request.POST, request.FILES, prefix='order', instance=order, initial={'requester': username.name()})
+        order_form = OrderForm(request.POST, request.FILES, prefix='order', instance=order, initial={'requester': request.user})
         orderlineformset = OrderLineInlineFormSet(
             request.POST, prefix='orderlines', instance=order)
 
@@ -255,7 +255,7 @@ def create_order(request, copy_id=None):
             order.pk = None
 
         else:
-            order_form = OrderForm(prefix='order', instance=order, initial={'requester': username.name()})
+            order_form = OrderForm(prefix='order', instance=order, initial={'requester': request.user})
             orderlineformset = OrderLineInlineFormSet(
                 prefix='orderlines', instance=order)
     
