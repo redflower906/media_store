47a1c7c733b8d48cab97b61c1d83bc50aab53c3e
Harrison <harrisons1@hhmi.org>
2018-06-12 10:58:47 -0400

    trying to get submitter name rather than username

--- store/views.py
+++ store/views.py
@@ -200,7 +200,7 @@ def create_order(request, copy_id=None):
 
     order = Order()
     user = request.user
-    user2 = user.userprofile.last_name
+    user2 = user.userprofile.get().last_name
 
     if request.method == "POST":
         
