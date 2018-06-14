97b093691e24588ae2556b2fc8d494b9f4903957
Harrison <harrisons1@hhmi.org>
2018-06-14 14:34:00 -0400

    fixing syntax error

--- store/admin.py
+++ store/admin.py
@@ -22,7 +22,7 @@ class DepartmentAdmin(admin.ModelAdmin):
         return qs
 
 class InventoryAdmin(admin.ModelAdmin):
-    list_display = ('inventory_text', 'cost', 'notes', 'active', 'media_type')
+    list_display = ('inventory_text', 'cost', 'notes_iv', 'active', 'media_type')
     list_filter = ('inventory_text', 'media_type')
     search_fields = ['inventory_text', 'cost']
 
@@ -30,8 +30,8 @@ class InventoryAdmin(admin.ModelAdmin):
 #    list_display = []
 
 class VendorAdmin(admin.ModelAdmin):
-    list_display = ['email_address', 'company', 'contact', 'notes', 'active']
-    search_fields = ['email_address', 'company', 'contact', 'notes', 'active']
+    list_display = ['email_address', 'company', 'contact', 'notes_ven', 'active']
+    search_fields = ['email_address', 'company', 'contact', 'notes_ven', 'active']
 
 class UserProfile(admin.ModelAdmin):
     list_display = ('user', 'first_name', 'last_name', 'employee_id', 'department', 'is_active')
