454e34c9431909e0d2e115104c141070c211c961
Harrison <harrisons1@hhmi.org>
2018-06-20 10:37:34 -0400

    trying to fix broken statusform

--- store/views.py
+++ store/views.py
@@ -451,7 +451,7 @@ def view_order(request):
     compBill_queryset = orders.filter(status__icontains='Billed').filter(days_since_bill__lte = 31).order_by('date_billed').prefetch_related('orderline_set').exclude(
     orderline__inventory__id='686')
 
-    incomp = OrderStatusFormSet(queryset=incomp_queryset, prefix='incomp')
+    incomp = OrderStatusFormSet(queryset=incomp_queryset, prefix='incomptest')
     recur = OrderStatusFormSet(queryset=recur_queryset, prefix='recur')
     compNotBill = OrderStatusFormSet(queryset=compNotBill_queryset, prefix='compNotBill')
     compBill = OrderStatusFormSet(queryset=compBill_queryset, prefix='compBill')
