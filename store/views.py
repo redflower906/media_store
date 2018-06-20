b49e6b38b5f7d99853e4626c9567aca08b6bcf1c
Harrison <harrisons1@hhmi.org>
2018-06-20 16:35:14 -0400

    trying to fix pagination of orderstatusformset

--- store/views.py
+++ store/views.py
@@ -433,10 +433,10 @@ def view_order(request):
     compBill_queryset = orders.filter(status__icontains='Billed').filter(days_since_bill__lte = 31).order_by('date_billed').prefetch_related('orderline_set').exclude(
     orderline__inventory__id='686')
 
-    incomp = OrderStatusFormSet(queryset=incomp_queryset, prefix='incomp')
-    recur = OrderStatusFormSet(queryset=recur_queryset, prefix='recur')
-    compNotBill = OrderStatusFormSet(queryset=compNotBill_queryset, prefix='compNotBill')
-    compBill = OrderStatusFormSet(queryset=compBill_queryset, prefix='compBill')
+    # incomp = OrderStatusFormSet(queryset=incomp_queryset, prefix='incomp')
+    # recur = OrderStatusFormSet(queryset=recur_queryset, prefix='recur')
+    # compNotBill = OrderStatusFormSet(queryset=compNotBill_queryset, prefix='compNotBill')
+    # compBill = OrderStatusFormSet(queryset=compBill_queryset, prefix='compBill')
 
     if request.method == 'POST':
         # for each order category, check to see if the form had been updated and save
@@ -456,7 +456,7 @@ def view_order(request):
         if order_formset.has_changed() and order_formset.is_valid():
             order_formset.save()
 
-        #pagination
+    #pagination
     page = request.GET.get('page')
     paginatorI = Paginator(incomp_queryset, 10)
     paginatorR = Paginator(recur_queryset, 10)
