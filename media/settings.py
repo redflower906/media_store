de6d9cf15fdba25e41865e6dd88fad6e81d55129
Harrison <harrisons1@hhmi.org>
2018-06-27 16:16:01 -0400

    trying to figure out why copy_order view doesn't work

--- media/settings.py
+++ media/settings.py
@@ -50,10 +50,10 @@ if DEBUG:
 
 ALLOWED_HOSTS = ['*']
 # 'mediastore', 'mediastore.int.janelia.org', 'localhost'
-#Ask Emily or Jody about sending email ~FIX~
 EMAIL_FILE_PATH =  os.path.join(BASE_DIR, 'emails')
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
 EMAIL_SERVER = 'harrisons1@janelia.hhmi.org'
-EMAIL_HOST = '10.42.5.23'
+# EMAIL_HOST = '10.42.5.23'
 #port = 465
 
 MEDIA_URL = '/media/'
