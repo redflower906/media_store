bdb7e959ead93dba09924495768079cb32b56d21
Harrison <harrisons1@hhmi.org>
2018-06-27 14:33:51 -0400

    changing copy_id=none to id

--- store/views.py
+++ store/views.py
@@ -186,7 +186,7 @@ def __build_inventory_groups():
 
 
 @login_required(login_url='login')
-def create_order(request, copy_id=None):
+def create_order(request, copy_id=id):
 
     order = Order()
     user = request.user
