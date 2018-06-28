405249e06dc0fc43842a2bb1715e619c2d47a34d
Harrison <harrisons1@hhmi.org>
2018-06-28 14:20:25 -0400

    working on pre_save duplication

--- store/models.py
+++ store/models.py
@@ -419,13 +419,11 @@ def status_email(sender, instance, *args, **kwargs):
 
         if instance.is_recurring == True:
             order = Order.objects.get(pk=instance.id)
-            ol = order.orderline_set
+            ol = order.prefetch_related('orderline_set')
             order.id = None
             order.pk = None
             order.save()
-            ol.pk = None
-            ol.order = order
-            ol.save()
+            order.orderline.set(ol)
         # messages.success(request, 'Order #{0} has been completed'.format(instance.id)) can't get request within a signal. find something better ~FIX~
 
     instance.date_complete = date.today()
