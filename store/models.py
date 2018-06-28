2de90d26d401cf7bfb82c0632dbae979a73f8097
Harrison <harrisons1@hhmi.org>
2018-06-28 14:57:26 -0400

    working on pre_save duplication

--- store/models.py
+++ store/models.py
@@ -419,7 +419,7 @@ def status_email(sender, instance, *args, **kwargs):
 
         if instance.is_recurring == True:
             order = Order.objects.get(pk=instance.id)
-            ol = Order.objects.filter(pk=instance.id).prefetch_related('orderline_set')
+            ol = Order.objects.prefetch_related('orderline_set').get(pk=instance.id)
             order.id = None
             order.pk = None
             order.save()
