9aa98b0889d29b76adb185af01dcadfbdce2f2df
Harrison <harrisons1@hhmi.org>
2018-07-03 10:44:45 -0400

    trying to see if problem is cronjob or method

--- store/cron/recur_order_ending_soon.py
+++ store/cron/recur_order_ending_soon.py
@@ -12,23 +12,33 @@ def recur_end_email():
     today = date.today()
     orders = Order.objects.all()
 
-    for order in orders:
-        today_to_stop = (today - order.date_recurring_stop).days
-        domain = 'mediastore.int.janelia.org/order/edit/{0}'.format(order.id)
-        # if today_to_stop == 21: 
-        subject,from_email,to = 'Order #{0} Submitted'.format(order.id), 'mediafacility@janelia.hhmi.org', order.requester.email
-        context = Context({
-            'id': order.id,
-            'domain': domain,
-        })
-        m_plain = render_to_string('three_weeks.txt', context.flatten())
-        m_html = render_to_string('three_weeks.html', context.flatten())
-        email =EmailMultiAlternatives(
-        subject,
-        m_plain,
-        from_email,
-        [to],
-        cc=[order.submitter.user_profile.email_address], #add mediafacility email here ~FIX~
-        )
-        email.attach_alternative(m_html, "text/html")
-        email.send()
+    subject,from_email,to = 'test', 'mediafacility@janelia.hhmi.org', 'harrisons1@janelia.hhmi.org'
+    m_plain = render_to_string('test')
+    email =EmailMultiAlternatives(
+    subject,
+    m_plain,
+    from_email,
+    [to],
+    )
+    email.send()
+
+    # for order in orders:
+    #     today_to_stop = (today - order.date_recurring_stop).days
+    #     domain = 'mediastore.int.janelia.org/order/edit/{0}'.format(order.id)
+    #     # if today_to_stop == 21: 
+    #     subject,from_email,to = 'Order #{0} Submitted'.format(order.id), 'mediafacility@janelia.hhmi.org', order.requester.email
+    #     context = Context({
+    #         'id': order.id,
+    #         'domain': domain,
+    #     })
+    #     m_plain = render_to_string('three_weeks.txt', context.flatten())
+    #     m_html = render_to_string('three_weeks.html', context.flatten())
+    #     email =EmailMultiAlternatives(
+    #     subject,
+    #     m_plain,
+    #     from_email,
+    #     [to],
+    #     cc=[order.submitter.user_profile.email_address], #add mediafacility email here ~FIX~
+    #     )
+    #     email.attach_alternative(m_html, "text/html")
+    #     email.send()
