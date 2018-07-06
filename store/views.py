bd920a0c85d1358e5bf1bef63245af7be51580ab
Harrison <harrisons1@hhmi.org>
2018-07-06 07:34:15 -0400

    order by date_billed descending

--- store/views.py
+++ store/views.py
@@ -421,7 +421,7 @@ def view_order(request):
     status__icontains='Canceled').exclude(date_recurring_stop__lt = today).prefetch_related('orderline_set').exclude(orderline__inventory__id='686')
     compNotBill_queryset = orders.filter(status__icontains='Complete').exclude(date_billed__isnull=False).order_by('date_complete').prefetch_related('orderline_set').exclude(
     orderline__inventory__id='686')
-    compBill_queryset = orders.filter(status__icontains='Billed').filter(days_since_bill__lte = 31).order_by('date_billed').prefetch_related('orderline_set').exclude(
+    compBill_queryset = orders.filter(status__icontains='Billed').filter(days_since_bill__lte = 31).order_by('-date_billed').prefetch_related('orderline_set').exclude(
     orderline__inventory__id='686')
 
     #pagination
