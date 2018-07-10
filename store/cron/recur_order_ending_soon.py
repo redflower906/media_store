ca9d4d4338d6e8193f664e230e154911b8b5fb83
Harrison <harrisons1@hhmi.org>
2018-07-10 16:10:48 -0400

    get rid of unecessary field because I'm dumb

--- store/cron/recur_order_ending_soon.py
+++ store/cron/recur_order_ending_soon.py
@@ -10,7 +10,7 @@ def recur_end_email():
     today = date.today()
     orders = Order.objects.all()
     for order in orders:
-        if order.date_recurring_stop and order.recur_stop_email is False:
+        if order.date_recurring_stop:
             today_to_stop = (order.date_recurring_stop - today).days
             print(today_to_stop)
             if today_to_stop == 21:
@@ -32,5 +32,3 @@ def recur_end_email():
                 )
                 email.attach_alternative(m_html, "text/html")
                 email.send()
-                order.recur_stop_email == True
-                order.save(update_fields=['recur_stop_email'])
