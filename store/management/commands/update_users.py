0a9b27522015ca2c543c3b72604aabed1a22df13
Harrison <harrisons1@hhmi.org>
2018-07-17 11:15:02 -0400

    departments weren't updating correctly, changing update_users to try to fix

--- store/management/commands/update_users.py
+++ store/management/commands/update_users.py
@@ -328,12 +328,12 @@ def add_employee(emp, **kwargs):
     # we don't want to update these details if the skip_update flag has been
     # set for this employee.
     if not profile.skip_updates:
-        if profile.is_manager == True:
-            profile.department    = get_department(emp['COSTCENTER'], emp['SUPORGNAME'], deptmgr=user)
         profile.department    = get_department(emp['COSTCENTER'], emp['SUPORGNAME'])
         profile.manager       = get_manager(emp['MGRID'])
         profile.first_name    = user.first_name
         profile.last_name     = user.last_name
+        if profile.is_manager == False:
+            profile.department    = get_department(emp['COSTCENTER'], emp['SUPORGNAME'], deptmgr=profile.manager)
     else:
         message("Skipping profile updates for {0} {1}, skip_updates flag set on user profile.\n".format(user.first_name, user.last_name), 'warning')
 
