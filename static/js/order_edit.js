c1a9d3f01160f58493a29f7c25dae25a04e47d4b
Harrison <harrisons1@hhmi.org>
2018-09-25 07:08:18 -0400

    trying to fix cost in order_edit

--- static/js/order_edit.js
+++ static/js/order_edit.js
@@ -143,7 +143,7 @@ function calc_cost(qty, cost){
         return 0
     }    
     console.log(qty);
-    console.log((parseInt(qty) * parseFloat(cost)).toFixed(2), 'parseint, parsefloat, tofixed');
+    console.log((qty * parseFloat(cost)).toFixed(2), 'parsefloat, tofixed');
     return  (parseInt(qty) * parseFloat(cost)).toFixed(2)
 
 }
