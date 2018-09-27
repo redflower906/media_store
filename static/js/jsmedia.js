a89cc9ac0adf8e4e60d9a9978fa6f644199da8d4
Harrison <harrisons1@hhmi.org>
2018-09-27 14:08:17 -0400

    fixing changeallbill function

--- static/js/jsmedia.js
+++ static/js/jsmedia.js
@@ -118,7 +118,7 @@ function recurringAlert(){
 //Change all orders tagged as complete to billed
 function changeAllBill(){
     $('td.status Complete option[value="Billed"]').prop('selected', true);
-    console.log($('td.status Complete').children().children());
+    console.log($('td.status Complete').children('.form-control option:selected').val());
 };
 
 //Automatically choose department and project code based on requester
