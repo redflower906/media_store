6f21f2b38a4e5687594f3bd5e6b2d24bb7274dc3
Harrison <harrisons1@hhmi.org>
2018-10-09 13:50:36 -0400

    making delete button a cancel button instead

--- static/js/jsmedia.js
+++ static/js/jsmedia.js
@@ -122,7 +122,7 @@ function changeAllBill(){
 
 //User cancel an order
 function changeCancel(){
-    console.log($(this))
+    console.log($(this.closest('.status').val()))
     // $('td.statusComplete').children().val('Billed');
 };
 
