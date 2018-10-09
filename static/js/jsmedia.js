b455f53503e723dea8f7663fdf810934cf908841
Harrison <harrisons1@hhmi.org>
2018-10-09 14:07:58 -0400

    working on javascript for cancel button

--- static/js/jsmedia.js
+++ static/js/jsmedia.js
@@ -122,7 +122,7 @@ function changeAllBill(){
 
 //User cancel an order
 function changeCancel(){
-    console.log($(this).closest('td.status'))
+    console.log($(this).parent().siblings('.status').val())
     // $('td.statusComplete').children().val('Billed');
 };
 
