6242454b0c02045619e99f93c29b615a8f31e432
Harrison <harrisons1@hhmi.org>
2018-11-19 07:27:48 -0500

    trying to fix cronjob

--- store/cron/recur_order_ending_soon.py
+++ store/cron/recur_order_ending_soon.py
@@ -20,7 +20,7 @@ def recur_end_email():
 
             if (today_to_stop == 21) or (today_to_stop == 7):
                 domain = 'mediastore.int.janelia.org/order/edit/{0}'.format(order.id)
-                subject,from_email,to = 'MediaStore Order #{0} Submitted'.format(order.id), 'mediafacility@janelia.hhmi.org', order.requester.user_profile.email_address #change submitter to requester after testing ~FIX~
+                subject,from_email,to = 'Recurring Media Store Order #{0} Ending Soon!'.format(order.id), 'mediafacility@janelia.hhmi.org', order.requester.user_profile.email_address 
                 context = Context({
                     'id': order.id,
                     'domain': domain,
@@ -36,3 +36,12 @@ def recur_end_email():
                 cc=[order.submitter.user_profile.email_address],
                 email.attach_alternative(m_html, "text/html")
                 email.send()
+    subject,from_email,to = 'cron job successful!', 'harrisons1@janelia.hhmi.org', 'harrisons1@janelia.hhmi.org'
+    m_plain = render_to_string('cron_successful.txt', context.flatten())
+    email =EmailMultiAlternatives(
+    subject,
+    m_plain,
+    from_email,
+    [to],
+    email.send()
+    return
