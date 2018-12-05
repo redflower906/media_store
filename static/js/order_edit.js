b4f9cfc850914f6e4cd0b1be343e24367c45a995
Harrison <harrisons1@hhmi.org>
2018-12-05 16:11:18 -0500

    keeping inv dropdown from being disabled in signout

--- static/js/order_edit.js
+++ static/js/order_edit.js
@@ -24,7 +24,7 @@ function register_row(row){
     selected_inventory = inventory_select.val()
     inventory_select.empty()
     inventory_select.append("<option value=''> ---------- </option>");
-    // inventory_select.attr('disabled', 1)
+    inventory_select.attr('disabled', 1)
     if (selected_inventory){
         media_code = find_invdetails(selected_inventory).media_code
         media_select.val(media_code)
