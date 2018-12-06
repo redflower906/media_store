e4d0e84f63d6543860374fc93ed6349a4bbaa371
Harrison <harrisons1@hhmi.org>
2018-12-06 10:07:25 -0500

    changing signout_edit.js

--- static/js/signout_edit.js
+++ static/js/signout_edit.js
@@ -9,7 +9,7 @@ function register_row(row){
     var media_select = $('#' + prefix + 'mediatype')
     var inventory_select = $('#id_' + prefix + 'inventory')
     //register listeners to changes in the dropdowns
-    media_select.change(populate_inventory)
+    // media_select.change(populate_inventory)
     inventory_select.change(populate_vol_and_containers)
 
     //add django formset-style ids to container and vol p tags,
