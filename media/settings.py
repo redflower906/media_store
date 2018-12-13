4c41b7bbeb3fae18ede31dafb44232416b5c5121
Harrison <harrisons1@hhmi.org>
2018-12-13 11:08:35 -0500

    adding postgres to installed_apps for search

--- media/settings.py
+++ media/settings.py
@@ -91,6 +91,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 #    'django_extensions',
     'jquery',
     'djangoformsetjs',
