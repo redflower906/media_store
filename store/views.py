5f12a1514f9ac3d63dfa6fbe31d1151a6be6e043
Harrison <harrisons1@hhmi.org>
2018-12-19 08:06:46 -0500

    trying to fix search_form

--- store/views.py
+++ store/views.py
@@ -839,10 +839,9 @@ def search(request):
         report = Order.objects.filter(Q(submitter=user)|Q(requester=user))
     else:
         report = Order.objects.all()
-    search_form = OrderSearchForm(user)
 
     if request.method == 'POST':
-        search_form = OrderSearchForm(request.POST, user)
+        search_form = OrderSearchForm(request.POST)
         if search_form.is_valid():
             pass
             # if search_form.instance.search_date_from and search_form.instance.search_keyword:
@@ -880,6 +879,8 @@ def search(request):
             #    messages.error(request, "You didn't submit any dates or keywords to search")
         else:
             messages.error("invalid form")
+    else:
+        search_form = OrderSearchForm(request.GET)
     return render(request, 'store/search.html', {
         'user': user,
         'search_form': search_form,
