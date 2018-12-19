9678d823d36904f9bb08d6a26f5c8dcfbb0e2599
Harrison <harrisons1@hhmi.org>
2018-12-19 14:15:30 -0500

    OrderSearchForm has no attribute instance, must use cleaned_data.get

--- store/views.py
+++ store/views.py
@@ -921,7 +921,6 @@ def search(request):
                 else:
                     reports = report.filter(date_billed__range=[datefrom, dateto])
             elif keyword:
-                keyword = form.instance.search_keyword
                 reports = report.prefetch_related('orderline_set').filter(Q(submitter__icontains=keyword)|Q(requester__icontains=keyword)|Q(notes_order__icontains=keyword)|
                 Q(project_code__icontains=keyword)|Q(department__number__icontains=keyword)| Q(orderline__inventory__inventory_text__icontains=keyword))
             else:
