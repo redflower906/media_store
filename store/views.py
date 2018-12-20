b64b5bc556bb5d476594f6c31c7e3ad3f660e50e
Harrison <harrisons1@hhmi.org>
2018-12-20 09:56:01 -0500

    changing dateinput format

--- store/views.py
+++ store/views.py
@@ -933,6 +933,7 @@ def search(request):
     else:
         form = OrderSearchForm()
         reports = ''
+        date_type = ''
     return render(request, 'store/search.html', {
         'date_type': date_type,
         'user': user,
