6fbe48443f1a6c0f5be75561fc8222f224118319
Harrison <harrisons1@hhmi.org>
2018-12-20 16:14:46 -0500

    trying to export search

--- store/views.py
+++ store/views.py
@@ -949,12 +949,15 @@ def search(request, exportCSV):
                 response = HttpResponse(content_type='text/csv')
                 response['Content-Disposition'] = 'attachment; filename="search_export.csv"'
                 writer = csv.writer(response)
-                writer.writerow(['order_id', 'Requester', 'Submitter', 'Date_Submitted', 'Is_Recurring', 'Due_Date', 'Product', 'Qty', 'Unit_Price', 'Special_Instructions', 'Location'])
-                e_reports = reports.values_list('id','requester__username', 'submitter__username', 'date_created', 'is_recurring', 'due_date', 'orderline__inventory__inventory_text', 
-                'orderline__qty', 'orderline__inventory__cost', 'notes_order','location')
+                # writer.writerow(['order_id', 'Requester', 'Submitter', 'Date_Submitted', 'Is_Recurring', 'Due_Date', 'Product', 'Qty', 'Unit_Price', 'Special_Instructions', 'Location'])
+                # e_reports = reports.values_list('id','requester__username', 'submitter__username', 'date_created', 'is_recurring', 'due_date', 'orderline__inventory__inventory_text', 
+                # 'orderline__qty', 'orderline__inventory__cost', 'notes_order','location')
 
-                for report in e_reports:
-                    writer.writerow(report)
+                # for report in e_reports:
+                #     writer.writerow(report)
+
+                for report in reports:
+                    writer.writerow([report['order_id']])
 
                 return response  
         else:
