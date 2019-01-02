5efd5524fa0e66a40aa423fde3f7e82b0b7f7216
Harrison <harrisons1@hhmi.org>
2019-01-02 13:47:56 -0500

    installing tinymce

--- store/models.py
+++ store/models.py
@@ -21,6 +21,7 @@ import decimal
 from datetime import datetime, date, timedelta
 from dateutil import relativedelta
 from djrichtextfield.models import RichTextField
+from tinymce.models import HTMLField
 
 # Create your models here.
 
@@ -309,7 +310,7 @@ class OrderLine(models.Model):
     
 
 class Announcements(models.Model):
-   text = models.TextField()
+   text = HTMLField()
    show = models.BooleanField(default=False)
 
 
