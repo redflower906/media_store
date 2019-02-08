a7a236cf8ecbc05d15dcce137c155d8b5600ee3a
Harrison <harrisons1@hhmi.org>
2019-02-08 13:36:29 -0500

    q_object add is being weird

--- store/views.py
+++ store/views.py
@@ -1103,7 +1103,7 @@ def searchtest(request):
                     for x in field_choice:
                         lookup = '%s__icontains' % x
                         query = {lookup : key}
-                        q_object.add(Q(**query), Q_bool)
+                        q_object.add(Q(**query), Q.OR)
                     # q_object.add((Q(order__submitter__first_name__icontains=key)|Q(order__submitter__last_name__icontains=key)|Q(order__requester__last_name__icontains=key)|Q(
                     # order__requester__first_name__icontains=key)|Q(order__notes_order__icontains=key)|Q(order__project_code__hhmi_project_id__icontains=key)|Q(
                     # order__department__number__icontains=key)| Q(inventory__inventory_text__icontains=key) | Q(order__id__icontains=key) | Q(order__status__icontains=key)), Q_bool)
