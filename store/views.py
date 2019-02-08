3966f59aaaac4ac71ac459b94c35c727196aba5c
Harrison <harrisons1@hhmi.org>
2019-02-08 15:10:55 -0500

    sub submitter first and last names for submitter choice

--- store/views.py
+++ store/views.py
@@ -1081,7 +1081,7 @@ def searchtest(request):
             and_or = form.cleaned_data.get('and_or')
             field_choice = form.cleaned_data.get('field_choice')
                 
-            field_choice[:] = [('order__submitter__first_name', 'order__submitter__last_name') if x=='submitter' else x for x in field_choice]
+            field_choice[:] = ['order__submitter__first_name', 'order__submitter__last_name' if x=='submitter' else x for x in field_choice]
 
                 
 
