c27ad7c5fe8893ce833c088aa6f5e4804ba8db68
Harrison <harrisons1@hhmi.org>
2019-02-12 13:35:52 -0500

    working on AND search for searchtest

--- store/views.py
+++ store/views.py
@@ -1108,11 +1108,11 @@ def searchtest(request):
 
                 if '+' in keyword:
                     keys2 = keyword.replace(' ', '')
-                    keys = (l.split(',') for l in keys2.split('+') if l)
-                    # keys2 = keys1.split('+')
-                    # for string in keys2:
-                    #     keys0 = string.split(',')
-                    # keys = keys2
+                    # keys = (l.split(',') for l in keys2.split('+') if l)
+                    keys2 = keys1.split('+')
+                    for string in keys2:
+                        keys0 = string.split(',')
+                    keys = keys2
                     
 
 
