9bf8b4ec19f16ead8e1cb9f05a4d2f2d51566847
Harrison <harrisons1@hhmi.org>
2019-02-25 15:48:47 -0500

    working on auto SO order view and template

--- store/views.py
+++ store/views.py
@@ -868,9 +868,9 @@ def auto_bv_so(request):
         'submitter': user,'requester': 16020, 'department': 191, 'location': '2E.267', 'is_recurring': False, 'notes_order': 'Signout Remainder'})
         orderlineformsetB = OrderLineInlineFormSet(request.POST, prefix='orderlineB', instance=order, initial ={
         'inventory': 1245, 'qty': remainderBottles})
-        orderlineformsetV = OrderLineInlineFormSet(request.POST, prefix='orderlineV', instance=order, initial ={
-        'inventory': 1263, 'qty': remainderVials})
-        if order_form.is_valid() and orderlineformsetB.is_valid() and orderlineformsetV.is_valid():
+        # orderlineformsetV = OrderLineInlineFormSet(request.POST, prefix='orderlineV', instance=order, initial ={
+        # 'inventory': 1263, 'qty': remainderVials})
+        if order_form.is_valid() and orderlineformsetB.is_valid():
             order = order_form.save(commit=False)
             order.status = 'Complete'
             order.save()
@@ -886,14 +886,14 @@ def auto_bv_so(request):
         'submitter': user,'requester': 16020, 'department': 191, 'location': '2E.267', 'is_recurring': False, 'notes_order': 'Signout Remainder'})
         orderlineformsetB = OrderLineInlineFormSet(request.POST, prefix='orderlineB', instance=order, initial ={
         'inventory': 1245, 'qty': remainderBottles})
-        orderlineformsetV = OrderLineInlineFormSet(request.POST, prefix='orderlineV', instance=order, initial ={
-        'inventory': 1263, 'qty': remainderVials})
+        # orderlineformsetV = OrderLineInlineFormSet(request.POST, prefix='orderlineV', instance=order, initial ={
+        # 'inventory': 1263, 'qty': remainderVials})
 
     return render(request,
         'store/autoform.html',{
         'order_form': order_form,
         'formsetB': orderlineformsetB,
-        'formsetV': orderlineformsetV,
+        # 'formsetV': orderlineformsetV,
         'user': user,
         'remainderBottles': remainderBottles,
         'remainderVials': remainderVials,
