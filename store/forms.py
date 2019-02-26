0fefaa08f0a764939368107a8b20237e2b397cc4
Harrison <harrisons1@hhmi.org>
2019-02-26 10:44:31 -0500

    removing extra=1 from orderlineinlineformset

--- store/forms.py
+++ store/forms.py
@@ -149,7 +149,7 @@ class OrderLineInlineFormSet(
         'line_cost': NumInput(attrs={'step': 'any', 'class': 'form-control col-centered line_calc line_cost', 'readonly':'readonly'}),
         'inventory': forms.Select(attrs={'class': 'form-control'}),
         },
-        extra=1, can_delete=True
+        can_delete=True
         )):
 
 
