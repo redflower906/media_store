81ba5fb46e392f1dd3c62067703f6b07f0d21fd5
Harrison <harrisons1@hhmi.org>
2019-02-26 10:32:40 -0500

    got key error at line 0, removed initial orderlineformset data

--- store/views.py
+++ store/views.py
@@ -871,7 +871,7 @@ def auto_bv_so (request):
         order_form = OrderForm(request.POST, request.FILES, prefix='order', instance=order, initial={
             'submitter': user,'requester': 16020, 'department': 191, 'location': '2E.267', 'is_recurring': False, 'notes_order': 'Signout Remainder'})
         orderlineformset = OrderLineInlineFormSet(
-            request.POST, prefix='orderlines', instance=order, initial ={'inventory': 1245, 'qty': remainderBottles})
+            request.POST, prefix='orderlines', instance=order)
 
         if order_form.is_valid() and orderlineformset.is_valid():
             order_form.save()
@@ -886,7 +886,7 @@ def auto_bv_so (request):
         order_form = OrderForm(prefix='order', instance=order, initial={
         'submitter': user,'requester': 16020, 'department': 191, 'location': '2E.267', 'is_recurring': False, 'notes_order': 'Signout Remainder'})
         orderlineformset = OrderLineInlineFormSet(
-            prefix='orderlines', instance=order, initial ={'inventory': 1245, 'qty': remainderBottles})
+            prefix='orderlines', instance=order)
 
 
     return render(request, 'store/autoform.html', {
