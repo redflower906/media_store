ac4c8817d4a59394c51c10335e83f0dc4cac5c35
Harrison <harrisons1@hhmi.org>
2019-02-26 10:29:33 -0500

    adding inital data to orderform and orderlineformset

--- store/views.py
+++ store/views.py
@@ -869,9 +869,9 @@ def auto_bv_so (request):
     if request.method == "POST":
 
         order_form = OrderForm(request.POST, request.FILES, prefix='order', instance=order, initial={
-            'submitter': request.user, 'requester': request.user, 'department': user.user_profile.department, 'project_code': user.user_profile.hhmi_project_id})
+            'submitter': user,'requester': 16020, 'department': 191, 'location': '2E.267', 'is_recurring': False, 'notes_order': 'Signout Remainder'})
         orderlineformset = OrderLineInlineFormSet(
-            request.POST, prefix='orderlines', instance=order)
+            request.POST, prefix='orderlines', instance=order, initial ={'inventory': 1245, 'qty': remainderBottles})
 
         if order_form.is_valid() and orderlineformset.is_valid():
             order_form.save()
@@ -884,9 +884,9 @@ def auto_bv_so (request):
 
     else:
         order_form = OrderForm(prefix='order', instance=order, initial={
-        'submitter': request.user, 'requester': request.user, 'department': user.user_profile.department, 'project_code': user.user_profile.hhmi_project_id})
+        'submitter': user,'requester': 16020, 'department': 191, 'location': '2E.267', 'is_recurring': False, 'notes_order': 'Signout Remainder'})
         orderlineformset = OrderLineInlineFormSet(
-            prefix='orderlines', instance=order)
+            prefix='orderlines', instance=order, initial ={'inventory': 1245, 'qty': remainderBottles})
 
 
     return render(request, 'store/autoform.html', {
