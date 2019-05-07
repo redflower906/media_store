d0ca473f76679147b175ceb71787cc69090eeac9
Harrison <harrisons1@hhmi.org>
2019-05-07 16:22:21 -0400

    if anything in request, don't autofill with submitter

--- static/js/jsmedia.js
+++ static/js/jsmedia.js
@@ -210,38 +210,37 @@ $(function autoChoose(){
         });
         $('#id_order-submitter').change(function() {
             if($('#id_order-requester').find("option:selected").val()){
-                var request = $('#id_order-requester_chosen').children('.chosen-single').text();
-                console.log('requester!');
-                console.log(request);
             }
-            var optionSelectedSub = $(this).find("option:selected");
-            var valueSelectedSub = optionSelectedSub.val();
-            var submitter_name = optionSelectedSub.text();
-            $.ajax({
-            url: '/ajax',
-            data: {
-                'id': valueSelectedSub,
-                'name': submitter_name
-            },
-            dataType: 'json',
-            success: function(data){
-                if (data.r_id) {
-                    $('#id_order-requester').val(data['r_id']).trigger("chosen:updated");
-                    $("#id_order-department").val(data['d_id']).trigger("chosen:updated");
+            else{
+                var optionSelectedSub = $(this).find("option:selected");
+                var valueSelectedSub = optionSelectedSub.val();
+                var submitter_name = optionSelectedSub.text();
+                $.ajax({
+                url: '/ajax',
+                data: {
+                    'id': valueSelectedSub,
+                    'name': submitter_name
+                },
+                dataType: 'json',
+                success: function(data){
+                    if (data.r_id) {
+                        $('#id_order-requester').val(data['r_id']).trigger("chosen:updated");
+                        $("#id_order-department").val(data['d_id']).trigger("chosen:updated");
 
-                    if(data.p_id){
-                        $("#id_order-project_code").val(data['up_id']).trigger("chosen:updated");
+                        if(data.p_id){
+                            $("#id_order-project_code").val(data['up_id']).trigger("chosen:updated");
+                        }
+                        else{
+                            $("#id_order-project_code").val('').trigger("chosen:updated");                    
+                        }
                     }
                     else{
-                        $("#id_order-project_code").val('').trigger("chosen:updated");                    
+                        $('#id_order-requester').val('').trigger("chosen:updated");
+                        $("#id_order-department").val('').trigger("chosen:updated");
                     }
-                }
-                else{
-                    $('#id_order-requester').val('').trigger("chosen:updated");
-                    $("#id_order-department").val('').trigger("chosen:updated");
-                }
-            },
-            });
+                },
+                });
+            }
         });
         
 
