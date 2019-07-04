ea026c551d9cc45def8da671605b5d40c337db68
Harrison <harrisons1@hhmi.org>
2019-07-04 09:52:06 -0400

    comment out SO_test in urls

--- media/urls.py
+++ media/urls.py
@@ -77,7 +77,7 @@ urlpatterns = [
     url(r'^signout/remainder', views.sign_outs_remainder, name='remainder'),
     url(r'^signout/new', views.create_signout, name='create_signout'),
     url(r'^signout/remtest', views.auto_bv_so, name='remtest'),    
-    url(r'^signout/testnew', views.SO_test, name='SO_test'),
+    #url(r'^signout/testnew', views.SO_test, name='SO_test'),
 
 #tiny mce?
     url(r'^tinymce/', include('tinymce.urls')),
