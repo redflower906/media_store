3f9bcd27e9f426c061c0030354c153773170f5a3
Harrison <harrisons1@hhmi.org>
2019-07-04 10:06:09 -0400

    forgot to change location of id-dept-show js in jsmedia

--- static/js/jsmedia.js
+++ static/js/jsmedia.js
@@ -276,6 +276,8 @@ $(function autoChoose(){
                     if (data.r_id) {
                         $('#id_order-requester').val(data['r_id']).trigger("chosen:updated");
                         $("#id_order-department").val(data['d_id']).trigger("chosen:updated");
+                        $("#id-dept-show").val(data['d_id']).trigger("chosen:updated");
+
 
                         if(data.p_id){
                             $("#id_order-project_code").val(data['up_id']).trigger("chosen:updated");
@@ -287,6 +289,8 @@ $(function autoChoose(){
                     else{
                         $('#id_order-requester').val('').trigger("chosen:updated");
                         $("#id_order-department").val('').trigger("chosen:updated");
+                        $("#id-dept-show").val('').trigger("chosen:updated");
+
                     }
                 }
                 });
