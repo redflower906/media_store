29b35b01b3ddf07dded9fa5143e22f960c36fb28
Harrison <harrisons1@hhmi.org>
2019-07-18 14:49:25 -0400

    playing with jquery to fix pagination

--- static/js/jsmedia.js
+++ static/js/jsmedia.js
@@ -389,13 +389,3 @@ $(function(){
     });
 });
 
-// temp pagination fix
-$(function(){
-    var arr = []
-    $('.pagination').each(function(){
-        var pag = $('.pagination').attr('id');
-        arr.push(pag);
-    });
-    console.log(arr);
-    console.log('is this working at all?')
-})
