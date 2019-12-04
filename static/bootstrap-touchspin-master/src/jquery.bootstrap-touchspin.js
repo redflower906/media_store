c9ad66a33a6f3396ee4cece8f398d92ef8fcad13
Harrison <harrisons1@hhmi.org>
2019-12-04 16:17:55 -0500

    changing touchspin.js

--- static/bootstrap-touchspin-master/src/jquery.bootstrap-touchspin.js
+++ static/bootstrap-touchspin-master/src/jquery.bootstrap-touchspin.js
@@ -240,23 +240,23 @@
           prefixhtml = '<span class="input-group-addon input-group-prepend bootstrap-touchspin-prefix input-group-prepend bootstrap-touchspin-injected"><span class="input-group-text">' + settings.prefix + '</span></span>',
           postfixhtml = '<span class="input-group-addon input-group-append bootstrap-touchspin-postfix input-group-append bootstrap-touchspin-injected"><span class="input-group-text">' + settings.postfix + '</span></span>';
 
-        if (prev.hasClass('input-group-btn') || prev.hasClass('input-group-prepend')) {
-          downhtml = '<button class="' + settings.buttondown_class + ' bootstrap-touchspin-down bootstrap-touchspin-injected" type="button">' + settings.buttondown_txt + '</button>';
-          prev.append(downhtml);
-        }
-        else {
-          downhtml = '<span class="input-group-btn input-group-prepend bootstrap-touchspin-injected"><button class="' + settings.buttondown_class + ' bootstrap-touchspin-down" type="button">' + settings.buttondown_txt + '</button></span>';
+        // if (prev.hasClass('input-group-btn') || prev.hasClass('input-group-prepend')) {
+        //   downhtml = '<button class="' + settings.buttondown_class + ' bootstrap-touchspin-down bootstrap-touchspin-injected" type="button">' + settings.buttondown_txt + '</button>';
+        //   prev.append(downhtml);
+        // }
+        // else {
+          downhtml = '<span class="input-group-btn-prepend bootstrap-touchspin-injected"><button class="' + settings.buttondown_class + ' bootstrap-touchspin-down" type="button">' + settings.buttondown_txt + '</button></span>';
           $(downhtml).insertBefore(originalinput);
-        }
+        //}
 
-        if (next.hasClass('input-group-btn') || next.hasClass('input-group-append')) {
-          uphtml = '<button class="' + settings.buttonup_class + ' bootstrap-touchspin-up bootstrap-touchspin-injected" type="button">' + settings.buttonup_txt + '</button>';
-          next.prepend(uphtml);
-        }
-        else {
-          uphtml = '<span class="input-group-btn input-group-append bootstrap-touchspin-injected"><button class="' + settings.buttonup_class + ' bootstrap-touchspin-up" type="button">' + settings.buttonup_txt + '</button></span>';
+        // if (next.hasClass('input-group-btn') || next.hasClass('input-group-append')) {
+        //   uphtml = '<button class="' + settings.buttonup_class + ' bootstrap-touchspin-up bootstrap-touchspin-injected" type="button">' + settings.buttonup_txt + '</button>';
+        //   next.prepend(uphtml);
+        // }
+        //else {
+          uphtml = '<span class="input-group-btn-append bootstrap-touchspin-injected"><button class="' + settings.buttonup_class + ' bootstrap-touchspin-up" type="button">' + settings.buttonup_txt + '</button></span>';
           $(uphtml).insertAfter(originalinput);
-        }
+        //}
 
         $(prefixhtml).insertBefore(originalinput);
         $(postfixhtml).insertAfter(originalinput);
