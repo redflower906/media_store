440b5171575be4e0b15fb9dfdc4b7bbdb4f2f61f
Harrison <harrisons1@hhmi.org>
2019-12-04 16:28:58 -0500

    commenting out keyup keydown touchspin

--- static/bootstrap-touchspin-master/src/jquery.bootstrap-touchspin.js
+++ static/bootstrap-touchspin-master/src/jquery.bootstrap-touchspin.js
@@ -352,45 +352,45 @@
           originalinput.val(settings.callback_after_calculation(originalinput.val()));
         });
 
-        elements.down.on('keydown', function(ev) {
-          var code = ev.keyCode || ev.which;
+        // elements.down.on('keydown', function(ev) {
+        //   var code = ev.keyCode || ev.which;
 
-          if (code === 32 || code === 13) {
-            if (spinning !== 'down') {
-              downOnce();
-              startDownSpin();
-            }
-            ev.preventDefault();
-          }
-        });
+        //   if (code === 32 || code === 13) {
+        //     if (spinning !== 'down') {
+        //       downOnce();
+        //       startDownSpin();
+        //     }
+        //     ev.preventDefault();
+        //   }
+        // });
 
-        elements.down.on('keyup.touchspin', function(ev) {
-          var code = ev.keyCode || ev.which;
+        // elements.down.on('keyup.touchspin', function(ev) {
+        //   var code = ev.keyCode || ev.which;
 
-          if (code === 32 || code === 13) {
-            stopSpin();
-          }
-        });
+        //   if (code === 32 || code === 13) {
+        //     stopSpin();
+        //   }
+        // });
 
-        elements.up.on('keydown.touchspin', function(ev) {
-          var code = ev.keyCode || ev.which;
+        // elements.up.on('keydown.touchspin', function(ev) {
+        //   var code = ev.keyCode || ev.which;
 
-          if (code === 32 || code === 13) {
-            if (spinning !== 'up') {
-              upOnce();
-              startUpSpin();
-            }
-            ev.preventDefault();
-          }
-        });
+        //   if (code === 32 || code === 13) {
+        //     if (spinning !== 'up') {
+        //       upOnce();
+        //       startUpSpin();
+        //     }
+        //     ev.preventDefault();
+        //   }
+        // });
 
-        elements.up.on('keyup.touchspin', function(ev) {
-          var code = ev.keyCode || ev.which;
+        // elements.up.on('keyup.touchspin', function(ev) {
+        //   var code = ev.keyCode || ev.which;
 
-          if (code === 32 || code === 13) {
-            stopSpin();
-          }
-        });
+        //   if (code === 32 || code === 13) {
+        //     stopSpin();
+        //   }
+        // });
 
         elements.down.on('mousedown.touchspin', function(ev) {
           elements.down.off('touchstart.touchspin');  // android 4 workaround
