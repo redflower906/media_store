a06f21ac721321a6b6beb62e3a2eb79fdbf51da7
Harrison <harrisons1@hhmi.org>
2019-12-10 14:26:52 -0500

    changing a thing in touchspin.js

--- static/bootstrap-touchspin-master/src/jquery.bootstrap-touchspin.js
+++ static/bootstrap-touchspin-master/src/jquery.bootstrap-touchspin.js
@@ -646,7 +646,7 @@
         //elements.input.val(settings.callback_after_calculation(Number(value).toFixed(2)));
         //elements.input.val(Number(value).toFixed(2));
         $('#id_orderlines-0-qty').val((parseFloat(value)).toFixed(2));
-        console.log($(':focus').parent());
+        console.log($(this));
 
         
         if (initvalue !== value) {
