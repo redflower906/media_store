41e1c77bd19705b1a5bca6fa82968b0fc1045ffc
Harrison <harrisons1@hhmi.org>
2019-12-10 14:19:31 -0500

    changing a thing in singout_edit.js

--- static/js/signout_edit.js
+++ static/js/signout_edit.js
@@ -18,7 +18,7 @@ function register_row(row){
     row.find('.inv_vol p').attr('id', prefix + 'vol')
 
     //register a listener to changes in the qty value
-    $('#id_' + prefix + 'qty').on('input', handle_qty_update)
+    $('#id_' + prefix + 'qty').change('input', handle_qty_update)
 
 
 }
