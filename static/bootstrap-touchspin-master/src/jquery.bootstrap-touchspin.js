530a1c53ed7b68e5b5c9c9bf51d8c96bfcd48102
Harrison <harrisons1@hhmi.org>
2019-12-10 13:56:28 -0500

    changing a thing in touchspin.js

--- static/bootstrap-touchspin-master/src/jquery.bootstrap-touchspin.js
+++ static/bootstrap-touchspin-master/src/jquery.bootstrap-touchspin.js
@@ -646,7 +646,7 @@
         //elements.input.val(settings.callback_after_calculation(Number(value).toFixed(2)));
         //elements.input.val(Number(value).toFixed(2));
         var test = elements.input.val();
-        console.log($(this).find(':input'));
+        console.log($(this).find(':input').val());
 
         
         if (initvalue !== value) {
