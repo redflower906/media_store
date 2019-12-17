1b6b5d2f6e7b98ab59de4bc48a8cd88429688750
Harrison <harrisons1@hhmi.org>
2019-12-17 10:08:06 -0500

    adding to JS in signout_edit2

--- static/js/signout_edit2.js
+++ static/js/signout_edit2.js
@@ -95,8 +95,8 @@ function handle_qty_update(invItem, qtyVal){
         console.log(inventory_id);
         var item = find_invdetails(inventory_id)
         console.log(item.cost);
-        console.log($('#id_' + prefix + 'line_cost'));
-        $('#id_' + prefix + 'line_cost').val(calc_cost(cur_quantity, item.cost));
+        console.log($('#' + prefix + 'line_cost'));
+        $('#' + prefix + 'line_cost').val(calc_cost(cur_quantity, item.cost));
         update_total();
     }
 
