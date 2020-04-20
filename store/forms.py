6a2297d2c6e69bc7f5cd416cbf5fa60b3e178722
Scarlett <coffmansr906@gmail.com>
2020-04-19 21:08:46 -0400

    move cleaned_data under for loop

--- store/forms.py
+++ store/forms.py
@@ -234,8 +234,8 @@ class AnnouncementsForm(forms.ModelForm):
 class BaseOrderStatusFormSetTest(BaseModelFormSet):
      def has_changed(self):
          hasChanged = super(BaseOrderStatusFormSetTest, self).has_changed()
-         cleaned_data = super(BaseOrderStatusFormSetTest, self).clean()
          for form in self.forms:
+            cleaned_data = super(BaseOrderStatusFormSetTest, self).clean()
             isChanged = cleaned_data.get('is_changed')
          if isChanged = hasChanged:
             return True
